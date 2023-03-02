47370b7c7ede2da5236b7d1263d5cf1c3916003a
Byeongji Lim <limb2@miamioh.edu>
2023-03-02 17:36:35 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <div className='searchBar'>
+          <input type="text" />
+        </div>
+        <div className='menu'></div>
+      </header>
+      <main>
+
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
