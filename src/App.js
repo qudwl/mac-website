47200dc41ec034a18cf0f5fd4272306de4691943
Byeongji Lim <limb2@miamioh.edu>
2023-03-05 00:37:11 -0500

    Adding simple design

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import './App.css';
+import "./App.css";
+import Logo from "./Logo";
+import { Search } from "react-bootstrap-icons";
 
 function App() {
+  const times = [];
+  const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  for (let i = 8; i < 13; i++) {
+    times.push(<span>{i + ":00"}</span>);
+  }
+  for (let i = 0; i < days.length; i++) {
+    days[i] = <span>{days[i]}</span>;
+  }
+
   return (
     <div className="App">
       <header>
-        <div className='searchBar'>
-          <input type="text" />
+        <div className="logo">
+          <Logo width="50" />
         </div>
-        <div className='menu'></div>
+        <div className="searchBar">
+          <input type="text" />
+          <button>
+            <Search size={"16px"} />
+            <span>Search</span>
+          </button>
+        </div>
+        <div className="menu">
+          <button>Export</button>
+          <button>CRN</button>
+        </div>
       </header>
       <main>
-
+        <div className="days">{days}</div>
+        <div className="times">{times}</div>
       </main>
       <footer></footer>
     </div>
