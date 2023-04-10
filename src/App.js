80c74f453e0325951b2cd049f719346d3f939bcb
Byeongji Lim <me@qudwl.com>
2023-04-10 17:02:56 -0400

    Fix error

--- src/App.js
+++ src/App.js
@@ -3,20 +3,13 @@ import React, { useEffect } from "react";
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import Dialog from "./Components/Dialog";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  addClassToSchedule,
-  changeDialogContent,
-  changeDialogState,
-  setTerm,
-  setSemesters,
-} from "./Redux/slice";
+import { useDispatch } from "react-redux";
+import { addClassToSchedule, setTerm, setSemesters } from "./Redux/slice";
 import Sheet from "@mui/joy/Sheet";
 import { getTerms } from "./Scripts/api-connector";
 import { downloadTerm } from "./Scripts/script";
 
 function App() {
-  const dark = useSelector((state) => state.slice.dark);
   const dispatch = useDispatch();
   useEffect(() => {
     const curClassesLocal = localStorage.getItem("curClasses");
@@ -31,7 +24,7 @@ function App() {
       dispatch(setSemesters(res));
       const curTerm = localStorage.getItem("curTerm");
       if (curTerm == null) {
-        const cur = res.findLast((t) => t.termId % 10 == 0);
+        const cur = res.findLast((t) => t.termId % 10 === 0);
         localStorage.setItem("curTerm", JSON.stringify(cur));
         dispatch(setTerm(cur));
         downloadTerm(res.termId);
--- src/Components/Days.js
+++ src/Components/Days.js
@@ -5,11 +5,12 @@ import { useSelector } from "react-redux";
 
 const Days = () => {
   const term = useSelector((state) => state.slice.curTerm);
-  const termNameArr = term.name.split(" ");
+
+  const termNameArr = term.name && term.name.split(" ");
   return (
     <div className="days">
       <Typography>
-        {term &&
+        {term.name &&
           `${termNameArr[0]} ${termNameArr[2].substring(
             termNameArr[2].length - 2
           )}`}
